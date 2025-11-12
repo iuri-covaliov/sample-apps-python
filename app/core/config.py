8e73656ac369533fcc08e8ca1a817692227c4e19
Iuri Covaliov <yurkov74@gmail.com>
2025-11-12 12:09:25 +0100

    feat(barebones): app dockerized

--- app/core/config.py
+++ app/core/config.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 from dotenv import load_dotenv
@@ -15,6 +16,7 @@ class AppSettings(BaseModel):
     NAME: str = "My Python App"
     DATA_DIR: Path = BASE_DIR / "data"
     DESCRIPTION: str = "Python App Barebones"
+    MODE: str = "DEV"  # DEV or PROD
     VERSION: str = "0.1.0"
 
 
@@ -37,5 +39,8 @@ class Settings(BaseSettings):
 
     def __init__(self) -> None:
         super().__init__()
+        # get app PORT from env var on production
+        if self.APP.MODE == "PROD":
+            self.APP.PORT = int(os.getenv("PORT", self.APP.PORT))
         # Ensure app data directories exist
         self.APP.DATA_DIR.mkdir(parents=True, exist_ok=True)
